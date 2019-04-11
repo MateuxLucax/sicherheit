096f75d07933e6d61a028f9a25cee794a1bfe8a6
Mateus <mateusslucass840@gmial.com>
2019-04-11 14:53:08 -0300

    v0.1.2

--- Sicherheit.sql
+++ Sicherheit.sql
@@ -1,27 +1,28 @@
-CREATE DATABASE IF NOT EXISTS 'Sicherheit';
+CREATE DATABASE IF NOT EXISTS `Sicherheit`;
 
-CREATE TABLE IF NOT EXISTS 'Clientes' (
-    'RG' VARCHAR(15) PRIMARY KEY,
-    'CPF' VARCHAR(15) NOT NULL,
-    'Nome' VARCHAR(150) NOT NULL,
-    'Endereço' VARCHAR(200) NOT NULL,
-    'Telefone' VARCHAR(25) NOT NULL
+USE `Sicherheit`
+
+CREATE TABLE IF NOT EXISTS `Clientes` (
+    `RG` VARCHAR(15) PRIMARY KEY,
+    `CPF` VARCHAR(15) NOT NULL,
+    `Nome` VARCHAR(150) NOT NULL,
+    `Endereço` VARCHAR(200) NOT NULL,
+    `Telefone` VARCHAR(25) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS 'Carros' (
-    'Placa' VARCHAR(7) PRIMARY KEY,
-    'Fabricante' VARCHAR(120) NOT NULL,
-    'Modelo' VARCHAR(120) NOT NULL,
-    'Ano' DATE NOT NULL,
-    'RG_Cliente' VARCHAR(15)  NOT NULL,
-    FOREIGN KEY ('RG_Cliente') REFERENCES 'Clientes'('RG')
+CREATE TABLE IF NOT EXISTS `Carros` (
+    `Placa` VARCHAR(7) PRIMARY KEY,
+    `Fabricante` VARCHAR(120) NOT NULL,
+    `Modelo` VARCHAR(120) NOT NULL,
+    `Ano` DATE NOT NULL,
+    `RG_Cliente` VARCHAR(15) NOT NULL,
+    FOREIGN KEY (`RG_Cliente`) REFERENCES `Clientes`(`RG`)
 );
 
-CREATE TABLE IF NOT EXISTS 'Ocorrencias' (
-    'Codigo' INT PRIMARY KEY AUTO_INCREMENT,
-    'Placa_Carro' VARCHAR(7) NOT NULL,
-    'Data' DATE NOT NULL,
-    'Local' VARCHAR(120) NOT NULL,
-    'Descricao' VARCHAR(120) NOT NULL
-    FOREIGN KEY ('Placa_Carro') REFERENCES 'Carros'('Placa')
+CREATE TABLE IF NOT EXISTS `Ocorrencias` (
+    `Codigo` INT PRIMARY KEY AUTO_INCREMENT
+    `Placa_Carro` VARCHAR(7) PRIMARY KEY,
+    `Data` DATE NOT NULL,
+    `Local` VARCHAR(120) NOT NULL,
+    `Descricao` VARCHAR(120) NOT NULL
 );
