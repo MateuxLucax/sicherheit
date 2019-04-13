617647ed3d97ca252c8829fdb0b001d6447e8f46
Mateus <mateusslucass840@gmial.com>
2019-04-13 14:53:44 -0300

    v0.4.1
    Adicionado página de clientes

--- Site/assets/js/init.js
+++ Site/assets/js/init.js
@@ -2,9 +2,23 @@
   $(function(){
 
     $('.sidenav').sidenav();
+    $('select').formSelect();
+    $('.modal').modal();
 
   });
 })(jQuery);
+
+$( "#pesquisaAvancada" ).hide();
+$(document).ready(function () {
+  var ckbox = $('#checkboxPesquisaAvancada');
+  $('input').on('click',function () {
+      if (ckbox.is(':checked')) {
+          $( "#pesquisaAvancada" ).show( "fast" );
+      } else {
+          $( "#pesquisaAvancada" ).hide( "fast" );
+      }
+  });
+});
 
 var d = new Date();
 var n = d.getFullYear();
