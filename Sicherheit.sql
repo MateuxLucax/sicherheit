53a30028a9da9ebdf33440d27b6fc90da1a05db8
Mateus <mateusslucass840@gmial.com>
2019-04-13 22:25:40 -0300

    v1.0
    SQL pronto para integração

--- Sicherheit.sql
+++ Sicherheit.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `Clientes` (
 
 CREATE TABLE IF NOT EXISTS `Carros` (
     `Placa` VARCHAR(7) PRIMARY KEY,
-    `Fabricante` VARCHAR(120) NOT NULL,
+    `Fabricante` VARCHAR(40) NOT NULL,
     `Modelo` VARCHAR(120) NOT NULL,
     `Ano` DATE NOT NULL,
     `RG_Cliente` VARCHAR(15) NOT NULL,
@@ -24,5 +24,6 @@ CREATE TABLE IF NOT EXISTS `Ocorrencias` (
     `Placa_Carro` VARCHAR(7) NOT NULL,
     `Data` DATE NOT NULL,
     `Local` VARCHAR(120) NOT NULL,
-    `Descricao` VARCHAR(120) NOT NULL
+    `Descricao` VARCHAR(240) NOT NULL
+    FOREIGN KEY (`Placa_Carro`) REFERENCES `Carros`(`Placa`);
 );
